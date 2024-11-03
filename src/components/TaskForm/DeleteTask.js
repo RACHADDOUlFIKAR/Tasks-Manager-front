3297321f21b6a42ca64927396ab1f97242db3d7e
Rachad Doulfikar <97551741+RACHADDOUlFIKAR@users.noreply.github.com>
2024-11-03 18:54:21 +0100

    Update DeleteTask.js

--- src/components/TaskForm/DeleteTask.js
+++ src/components/TaskForm/DeleteTask.js
@@ -1,11 +1,1 @@
-const handleDelete = (taskId) => {
-    const confirmDelete = window.confirm("Êtes-vous sûr de vouloir supprimer cette tâche ?");
-    
-    if (confirmDelete) {
-        axios.delete(`http://localhost:8000/api/tasks/${taskId}/`)
-            .then(() => {
-                setTasks(tasks.filter(task => task.id !== taskId));
-            })
-            .catch(error => console.error('Error deleting task:', error));
-    }
-};
+
